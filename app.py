4ff04fa9e4cb665f2f04767c3e309bde93227cee
DavidFrancoR <dfranco@franzier.com>
2022-07-26 23:27:42 +0000

    Creamos o template

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-
 from flask import Flask, render_template
+from datetime import datetime
+
 app = Flask("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello World"
+posts = [
+    {
+        "title": "O meu Primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Feulo",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu Segundo Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Danilo",
+        "created": datetime(2022,7,26)
+    },
+]
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
+#codigo de prueba nterior
+#@app.route("/hello")
+#def hello():
+#    return "Hello World"
+
+#@app.route("/meucontato")
+#def meuContato():
+#    return render_template('index.html')
